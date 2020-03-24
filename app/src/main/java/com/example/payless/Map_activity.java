80de6dc6127cd0b1afa9e5a5a6572213dc7b45e7
Alex <alexandr.bagno.junior@gmail.com>
2020-03-24 10:19:49 +0200

    Somethings change

--- app/src/main/java/com/example/payless/Map_activity.java
+++ app/src/main/java/com/example/payless/Map_activity.java
@@ -1,19 +1,35 @@
 package com.example.payless;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class Map_activity extends AppCompatActivity {
     ImageButton back;
+    private GoogleMap mMap;
+    private static final String TAG = Map_activity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_act);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
         configureBackButton();
+
 
     }
     private void configureBackButton(){
@@ -25,4 +41,5 @@ public class Map_activity extends AppCompatActivity {
             }
         });
     }
+
 }
