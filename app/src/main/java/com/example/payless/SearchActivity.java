66f629a8d548e4b6efa0d33648472a11963c3f4c
Alex <alexandr.bagno.junior@gmail.com>
2020-03-26 13:56:18 +0200

    Delete action bar

--- app/src/main/java/com/example/payless/SearchActivity.java
+++ app/src/main/java/com/example/payless/SearchActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -16,6 +17,7 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.search_act);
         configureNextButton();
 
