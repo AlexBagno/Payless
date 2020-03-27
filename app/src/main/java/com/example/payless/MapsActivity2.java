53e273fac6695ce638a1b7b6fd43ebb4f75ec3ca
Alex <alexandr.bagno.junior@gmail.com>
2020-03-27 20:20:11 +0200

    Add a marker on map and your location at map

--- app/src/main/java/com/example/payless/MapsActivity2.java
+++ app/src/main/java/com/example/payless/MapsActivity2.java
@@ -1,8 +1,13 @@
 package com.example.payless;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +15,9 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -18,6 +26,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +37,9 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
     ImageButton back;
     private GoogleMap map;
     SupportMapFragment mapFragment;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final  int REQUEST_CODE = 101;
 
     private List<Place> foundPlaces ;
 
@@ -36,17 +50,37 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps2);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-         mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        onMapReady(map);
-        createMapView();
-        addMarker();
-        configureBackButton();
+        /* mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);*/
+         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+         fetchLastLocation();
+       // onMapReady(map);
+       // createMapView();
+       // configureBackButton();
         foundPlaces = new ArrayList<Place>();
 
 
     }
+    private  void  fetchLastLocation(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+           ActivityCompat.requestPermissions(this, new String[]
+                   {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude()+""+currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    supportMapFragment.getMapAsync(MapsActivity2.this);
+                }
 
+            }
+        });
+    }
 
 
     /**
@@ -60,26 +94,41 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        this.map = googleMap;
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Your location");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        googleMap.addMarker(markerOptions).setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker));
+
 
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_CODE:
+                if (grantResults.length> 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    fetchLastLocation();
+                }
+                break;
+        }
+    }
 
-    void addMarker(){
+    /*void addMarker(){
         if(null != map){
             LatLng dnepr = new LatLng(48.45, 34.98);
             map.addMarker(new MarkerOptions().position(dnepr).title("Your town")).setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker));
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(dnepr,10));
         }
-    }
+    }*/
 
 
 
 
 
 
-    private void createMapView(){
+   /* private void createMapView(){
         try{
             if(map == null){
                 ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
@@ -105,7 +154,7 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
 
         }
 
-    }
+    }*/
 
 }
 
