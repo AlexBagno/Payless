8ea85e614420aab50b4ab7c4a29fe8dcb609727b
Alex <alexandr.bagno.junior@gmail.com>
2020-05-03 17:48:32 +0300

    Add button to search nearby places

--- app/src/main/java/com/example/payless/MapsShop.java
+++ app/src/main/java/com/example/payless/MapsShop.java
@@ -5,11 +5,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -23,23 +25,24 @@ import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class MapsShop extends FragmentActivity implements OnMapReadyCallback{
+public class MapsShop extends FragmentActivity implements OnMapReadyCallback  {
+
 
 
     private static final String TAG =  "MapsShop";
@@ -55,6 +58,9 @@ public class MapsShop extends FragmentActivity implements OnMapReadyCallback{
     private static final  int REQUEST_CODE = 101;
     private EditText mSearchText;
     private ImageView mGps;
+    int PROXIMITY_RADIUS =50000;
+    double latitude, longitude;
+    String apiKey = "AIzaSyChp6q5OUPaf3okmibXOKnNRGKC6hdrPOI";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -280,5 +286,134 @@ public class MapsShop extends FragmentActivity implements OnMapReadyCallback{
 
 
 
+    public void onClick (View view){
+        Object dataTransfer[] = new Object[2];
+        GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+
+        switch (view.getId()){
+            case R.id.cafe:
+                mMap.clear();
+                String cafe = "cafe";
+                String url = getUrl(latitude, longitude, cafe);
+                dataTransfer[0] = mMap;
+                dataTransfer[1] = url;
+
+                getNearbyPlacesData.execute(dataTransfer);
+                Toast.makeText(MapsShop.this, "Showing Nearby Cafe", Toast.LENGTH_SHORT).show();
+                break;
+
+
+            case R.id.store:
+                mMap.clear();
+                String shopping_mall = "shopping_mall";
+                url = getUrl(latitude, longitude, shopping_mall);
+                dataTransfer[0] = mMap;
+                dataTransfer[1] = url;
+
+                getNearbyPlacesData.execute(dataTransfer);
+                Toast.makeText(MapsShop.this, "Showing Nearby Store", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.resta:
+                mMap.clear();
+                String restaurant = "restaurant";
+                url = getUrl(latitude, longitude, restaurant);
+                dataTransfer[0] = mMap;
+                dataTransfer[1] = url;
+
+                getNearbyPlacesData.execute(dataTransfer);
+                Toast.makeText(MapsShop.this, "Showing Nearby Restaurants", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.clothing_store:
+                mMap.clear();
+                String clothing_store = "clothing_store";
+                url = getUrl(latitude, longitude, clothing_store);
+                dataTransfer[0] = mMap;
+                dataTransfer[1] = url;
+
+                getNearbyPlacesData.execute(dataTransfer);
+                Toast.makeText(MapsShop.this, "Showing Nearby Clothing Store", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+
+    private String getUrl(double latitude , double longitude , String nearbyPlace)
+    {
+        //latitude = currentLocation.getLatitude();
+       // longitude = currentLocation.getLongitude();
+
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.516,35.0525");
+       // googlePlaceUrl.append("location="+latitude+","+longitude);
+        googlePlaceUrl.append("&radius="+PROXIMITY_RADIUS);
+        googlePlaceUrl.append("&type="+nearbyPlace);
+        googlePlaceUrl.append("&key=AIzaSyCXvAe5yWBb4Nu1mCrzqKQep0VELdcxHRE");
+        googlePlaceUrl.append("&sensor=true");
+
+        Log.d(TAG, "url = "+googlePlaceUrl.toString());
+
+        return googlePlaceUrl.toString();
+    }
+
+
+
 
 }
+class GetNearbyPlacesData extends AsyncTask<Object, String, String> {
+
+    private String googlePlacesData;
+    private GoogleMap mMap;
+    String url;
+    private static final String TAG =  "MapsShop";
+
+
+    @Override
+    protected String doInBackground(Object... objects){
+        mMap = (GoogleMap)objects[0];
+        url = (String)objects[1];
+
+        DownloadURL downloadURL = new DownloadURL();
+        try {
+            googlePlacesData = downloadURL.readUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googlePlacesData;
+    }
+
+    @Override
+    protected void onPostExecute(String s){
+
+        List<HashMap<String, String>> nearbyPlaceList;
+        DataParser parser = new DataParser();
+        nearbyPlaceList = parser.parse(s);
+        Log.d(TAG,"called parse method");
+        showNearbyPlaces(nearbyPlaceList);
+    }
+
+    private void showNearbyPlaces(List<HashMap<String, String>> nearbyPlaceList)
+    {
+        for(int i = 0; i < nearbyPlaceList.size(); i++)
+        {
+            MarkerOptions markerOptions = new MarkerOptions();
+            HashMap<String, String> googlePlace = nearbyPlaceList.get(i);
+
+            String placeName = googlePlace.get("place_name");
+            String vicinity = googlePlace.get("vicinity");
+            double lat = Double.parseDouble( googlePlace.get("lat"));
+            double lng = Double.parseDouble( googlePlace.get("lng"));
+
+            LatLng latLng = new LatLng( lat, lng);
+            markerOptions.position(latLng);
+            markerOptions.title(placeName + " : "+ vicinity);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+            mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        }
+    }
+}
+
+
